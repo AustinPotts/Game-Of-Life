fe66a576555c0eb08563c3ecb3fa900027bf2ef8
AustinPotts <pottsevolvements@gmail.com>
2020-06-22 12:21:57 -0400

    Configured Project

---
+++ Game-Of-Life-iOS/Game-Of-Life-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game-Of-Life-iOS
+//
+//  Created by Austin Potts on 6/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
